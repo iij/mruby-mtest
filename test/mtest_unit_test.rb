282cfaf1eada1d74299e45d26c66127df3dfed98
Akira Yumiyama <yumiyama.akira@gmail.com>
2012-12-29 16:18:21 +0900

    update mruby-mtest

--- test/mtest_unit_test.rb
+++ test/mtest_unit_test.rb
@@ -2,57 +2,20 @@
 # Test of Minimal Test framework for mruby.
 #
 
-if Object.const_defined?(:MTest)
-  class Test4MTest < MTest::Unit::TestCase
-    def test_assert
-      assert(true)
-      assert(true, 'true sample test')
-    end
+assert('MTest defined') do
+  Object.const_defined?(:MTest)
+end
 
-    def test_assert_block
-      assert_block('msg') do
-        'something-block'
-      end
-    end
+assert('MTest::Unit defined') do
+  MTest.const_defined?(:Unit)
+end
 
-    def test_assert_empty
-      assert_empty('', 'string empty')
-      assert_empty([], 'array empty')
-      assert_empty({}, 'hash empty')
-    end
-
-    def test_assert_equal
-      assert_equal('', nil.to_s)
-    end
-
-    def test_assert_in_delta
-      assert_in_delta(0, 0.1, 0.5)
-    end
-
-    def test_assert_includes
-      assert_includes([1,2,3], 1)
-    end
-
-    def test_assert_instance_of
-      assert_instance_of Array, []
-      assert_instance_of Class, Array
-    end
-
-    def test_assert_kind_of
-      assert_kind_of Array, []
-      assert_kind_of Class, Array
-    end
-
-    def test_assert_match
-      assert_match 'abc', 'abc'
-    end
+class Test4MTest < MTest::Unit::TestCase
+  def test_assert
+    assert(true, 'true sample test')
   end
+end
 
-  if self.methods.include?('assert')
-    assert('MTest::Unit') do
-      MTest::Unit.new.run == 0
-    end
-  else
-    MTest::Unit.new.run == 0
-  end
+assert('Test4Mtest') do
+  MTest::Unit.new.run == 0
 end
